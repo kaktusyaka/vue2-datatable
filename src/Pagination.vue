1084963a355f543fd0dca20c3b55ae0741f1d43a
Vitalii <kaktusyaka@users.noreply.github.com>
2020-10-20 21:22:55 +1000

    change to tailwind

--- src/Pagination.vue
+++ src/Pagination.vue
@@ -1,24 +1,19 @@
 <template>
-  <ul class="pagination" style="margin: 0" name="Pagination">
-    <li v-if="!isFirstPage" class="page-item" @click="turnPage(-1)">
-      <a href="#" class="page-link" @click.prevent>
-        <i class="fa fa-arrow-left"></i>
-      </a>
-    </li>
-    <li v-for="i in dspBtns" :class="['page-item', { 'active': i === curPage }]">
-      <a v-if="i" href="#" class="page-link" @click.prevent="handleClick(i)">
-        {{ i }}
-      </a>
-      <a v-else class="page-link">
-        <i class="fa fa-ellipsis-h"></i>
-      </a>
-    </li>
-    <li v-if="!isLastPage" class="page-item" @click="turnPage(1)">
-      <a href="#" class="page-link" @click.prevent>
-        <i class="fa fa-arrow-right"></i>
-      </a>
-    </li>
-  </ul>
+  <nav class="pagination relative z-0 inline-flex shadow-sm" name="Pagination">
+    <a href="#" class="page-link relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-500 hover:text-gray-400 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150" v-if="!isFirstPage" @click="turnPage(-1)" @click.prevent>
+      <svg class="h-5 w-5" x-description="Heroicon name: chevron-left" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+        <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+      </svg>
+    </a>
+    <a v-if="i" v-for="i in dspBtns" href="#" class="page-link -ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150" @click.prevent="handleClick(i)">
+      {{ i }}
+    </a>
+    <a href="#" v-if="!isLastPage" @click="turnPage(1)" class="page-link -ml-px relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-500 hover:text-gray-400 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150" @click.prevent>
+      <svg class="h-5 w-5" x-description="Heroicon name: chevron-right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+        <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
+      </svg>
+    </a>
+  </nav>
 </template>
 <script>
 export default {
