4764953a45bca9299c6cc38bfcbcd59a278cc87e
Vitalii <kaktusyaka@users.noreply.github.com>
2020-10-20 21:32:02 +1000

    added active class

--- src/Pagination.vue
+++ src/Pagination.vue
@@ -5,7 +5,7 @@
         <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"></path>
       </svg>
     </a>
-    <a v-if="i" v-for="i in dspBtns" href="#" class="page-link -ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150" @click.prevent="handleClick(i)">
+    <a v-if="i" v-for="i in dspBtns" :class="['page-item', { 'active': i === curPage }]" href="#" class="page-link -ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150" @click.prevent="handleClick(i)">
       {{ i }}
     </a>
     <a href="#" v-if="!isLastPage" @click="turnPage(1)" class="page-link -ml-px relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-500 hover:text-gray-400 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150" @click.prevent>
