1ca02fc2a180f4d126ae44537631a91cabe5260e
Vitalii <kaktusyaka@users.noreply.github.com>
2020-10-20 21:28:45 +1000

    update classes

--- src/PageSizeSelect.vue
+++ src/PageSizeSelect.vue
@@ -1,6 +1,6 @@
 <template>
   <label name="PageSizeSelect">
-    <select class="form-control input-sm -page-size-select" v-model="query.limit"
+    <select class="mt-1 form-select block w-full pl-3 pr-10 py-2 text-base leading-6 border-gray-300 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5 -page-size-select" v-model="query.limit"
       @change="query.offset = 0 /* reset to the first page */">
       <option v-for="i in pageSizeOptions" :value="i">{{ i }}</option>
     </select>
